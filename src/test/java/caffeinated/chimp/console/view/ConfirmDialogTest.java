28ccf65db15d82aff81e18d8d60a7442b0189727
CaffeinatedChimp <RyanHKris@outlook.com>
2018-03-09 12:01:04 +0930

    Launched v3.3

---
+++ src/test/java/caffeinated/chimp/console/view/ConfirmDialogTest.java
@@ -0,0 +1,16 @@
+package caffeinated.chimp.console.view;
+
+public class ConfirmDialogTest {
+
+    public static void main(String[] args) {
+        ActionView actionView = new ActionView("ConfirmDialogTest", "") {
+            @Override
+            public void executeCustomAction() {
+                this.confirmDialog("Sure?");
+            }
+        };
+
+        actionView.display();
+
+    }
+}
